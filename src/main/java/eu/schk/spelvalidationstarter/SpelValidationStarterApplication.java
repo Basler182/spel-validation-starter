34bce31329bf140a834f969bc5d9273333482716
Basler182 <showdialog@web.de>
2025-11-23 18:56:58 +0100

    Initial commit

---
+++ src/main/java/eu/schk/spelvalidationstarter/SpelValidationStarterApplication.java
@@ -0,0 +1,13 @@
+package eu.schk.spelvalidationstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpelValidationStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpelValidationStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/schk/spelvalidationstarter/SpelValidationStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.schk.spelvalidationstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpelValidationStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
